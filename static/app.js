da60f95571c5ba3d972d69fa12cdd7b093747c9a
Vibhutha Kumarage <vkartz@gmail.com>
2020-12-06 23:02:40 +0530

    chartdata and chart options added

--- static/app.js
+++ static/app.js
@@ -1,5 +1,53 @@
 var socket = io("http://127.0.0.1:5000/");
 
+const chartOptions = {
+  responsive: false,
+  maintainAspectRatio: true,
+  title: {
+    display: true,
+    text: "",
+    fontSize: 18,
+    fontColor: "Black"
+  },
+  legand: {
+    display: false
+  },
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true,
+      },
+      gridLines: {
+        color: "black",
+        borderDash: [2, 5],
+      },
+      scaleLabel: {
+        display: true,
+        labelString: "kbps",
+        fontColor: "black"
+      }
+    }]
+  },
+  tooltips: {
+    enabled: false
+  }
+}
+
+const chartData = {
+  labels: [],
+  datasets: [{
+    label: "Matched",
+    borderColor: 'rgb(0, 0, 255)',
+    fill: false,
+    data: [],
+  }, {
+    label: "Dropped",
+    borderColor: 'rgb(255, 0, 0)',
+    fill: false,
+    data: [],
+  }]
+}
+
 const getQOSButton = document.querySelector("#getQos")
 getQOSButton.addEventListener('click', function () {
   console.log("Hello World");
