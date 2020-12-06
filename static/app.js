58d700630d4b8ba9c2f6762dd7a3642415d72ad5
Vibhutha Kumarage <vkartz@gmail.com>
2020-12-07 01:00:37 +0530

    charts genertaion modified

--- static/app.js
+++ static/app.js
@@ -48,7 +48,11 @@ const chartData = {
   }]
 }
 
+const qosValuesRoot = []
+
 const getQOSButton = document.querySelector("#getQos")
+const chartGrid = document.querySelector("#chart-grid")
+
 getQOSButton.addEventListener('click', function () {
   console.log("Hello World");
   socket.emit("getQOS");
@@ -62,10 +66,45 @@ socket.on("connect_response", function (msg) {
   console.log(msg);
 })
 
+// QOS status listner
 socket.on("qos_status", function (msg) {
-  console.log(msg);
+  let qosValues = msg["qos_values"]
+  console.log(qosValues)
+  for (let i = 0; i < qosValues.length; i++) {
+    updateChart(i, [qosValues[i][1], qosValues[i][2]])
+  }
 })
 
+function updateChart(chartObjectIndex, dataset) {
+  let chartObject = Chart.instances[chartObjectIndex]
+  console.log(chartObject.data.datasets[0].data)
+  try {
+    chartObject.data.datasets[0].data.push(dataset[0]);
+    chartObject.data.datasets[1].data.push(dataset[1]);
+    chartObject.data.labels = [...Array(chartObject.data.datasets[0].data.length).keys()]
+    chartObject.update();
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+// QOS information listener
 socket.on("qos_info", function (msg) {
-  console.log(msg);
+  console.log(msg)
+  let policyClasses = msg["policy_classes"]
+  for (let i = 0; i < policyClasses.length; i++) {
+    let row = `
+    <div>
+      <canvas id="chartIndex_${i}" width="500" height="300"></canvas>
+    </div>`
+    chartGrid.innerHTML += row
+  }
+  for (let i = 0; i < policyClasses.length; i++) {
+    let getChartElementForPolicy = document.getElementById(`chartIndex_${i}`)
+    new Chart(getChartElementForPolicy.getContext('2d'), {
+      type: 'line',
+      data: chartData,
+      options: chartOptions
+    });
+  }
 })
